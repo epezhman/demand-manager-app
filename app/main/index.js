ce7bf2fc8a985fff670c8eed46830bb726993b4f
Pezhman Nasirifard <epezhman@gmail.com>
2016-07-24 16:22:43 +0200

    working windows and linux

--- app/main/index.js
+++ app/main/index.js
@@ -58,7 +58,7 @@ app.on('quit', () => {
 app.on('ready', () => {
     if (!conf.get('first-time-start')) {
         windows.main.init(enums.WindowType.ABOUT)
-        conf.set('first-time-start', true)
+       conf.set('first-time-start', true)
     }
     tray.init()
     setTimeout(delayedStart, config.DELAY_START_TIME)
