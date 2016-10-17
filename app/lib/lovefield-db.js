19cd93aefbf0dd24c0cb3e630e6e0302f6798e4a
Pezhman Nasirifard <epezhman@gmail.com>
2016-10-17 17:19:27 +0200

    battery ubuntu

--- app/lib/lovefield-db.js
+++ app/lib/lovefield-db.js
@@ -55,7 +55,7 @@ var operations = {
             var batteryObject = {
                 'remaining_time_minutes': parseInt(params['win32_battery-estimatedruntime']),
                 'power_rate_w': discharge_rate > 0 ? discharge_rate : charge_rate,
-                'remaining_capacity_percent':parseInt(params['win32_battery-estimatedchargeremaining']),
+                'remaining_capacity_percent': parseInt(params['win32_battery-estimatedchargeremaining']),
                 'voltage_v': Math.round((parseInt(params['batterystatus-voltage']) / 1000) * 100) / 100,
                 'charging_bool': params['batterystatus-charging'].toLowerCase() === "true",
                 'discharging_bool': params['batterystatus-discharging'].toLowerCase() === "true",
@@ -70,9 +70,9 @@ var operations = {
             var batteryObject = {
                 'remaining_time_minutes': utils.hoursToMinutes(Math.round((parseInt(params['energynow'])
                         / parseInt(params['powernow'])) * 100) / 100),
-                'power_rate_w': parseInt(params['powernow'] / 1000000),
+                'power_rate_w': Math.round( parseInt(params['powernow'] / 10000)) / 100,
                 'remaining_capacity_percent': parseInt(params['capacity']),
-                'voltage_v': parseInt(params['voltage-now'] / 1000000),
+                'voltage_v': Math.round(parseInt(params['voltage-now'] / 10000)) / 100,
                 'charging_bool': params['status'] === 'Charging',
                 'discharging_bool': params['status'] === 'Discharging',
                 'ac_connected_bool': params['ac-connected'] === '1',
