000e2f2ec31c5f5123205e10040781da45a9f885
Pezhman Nasirifard <epezhman@gmail.com>
2016-11-03 00:21:35 +0100

    auto laucnh

--- app/lib/notify.js
+++ app/lib/notify.js
@@ -6,16 +6,6 @@ const config = require('../config')
 const windows = require('../main/windows')
 
 function notify(msg) {
+    windows.notify.init(msg)
 
-    if (config.IS_WINDOWS || config.IS_LINUX) {
-        windows.notify.init(msg)
-    }
-    else {
-        const notifier = require('node-notifier')
-        notifier.notify({
-            title: config.APP_NAME,
-            message: msg,
-            icon: config.APP_ICON
-        })
-    }
 }
